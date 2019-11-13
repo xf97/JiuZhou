b9436fab8f02c4e5ed52724024b664551e34e8de
xf97 <15850673022@163.com>
2019-11-13 22:09:08 +0800

    update test cases

---
+++ 050/SecurityProblem/Re-entrancy/gray contract/gray_re_entrancy.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.0;
+
+contract EtherStore{
+    
+    bool reEntrancyMutex = false;
+    uint256 public withdrawLimit = 1 ether;
+    mapping( address => uint256) public lastWithdrawTime;
+    mapping( address => uint256) public balances;
+    
+    function depositFunds() public payable{
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdrawFunds(uint256 _weiToWithdraw) public{
+        require(!reEntrancyMutex);
+        require(balances[msg.sender] >= _weiToWithdraw);
+        
+        require(_weiToWithdraw <= withdrawLimit);
+        
+        require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
+        balances[msg.sender] -= _weiToWithdraw;
+        lastWithdrawTime[msg.sender] = now;
+        
+        reEntrancyMutex = true;
+        msg.sender.call.value(_weiToWithdraw)("");
+        
+        reEntrancyMutex =false;
+    }
+}
