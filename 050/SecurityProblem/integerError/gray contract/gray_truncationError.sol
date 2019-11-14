d7be026c33650b46dc6492198bb6e1f8a6726660
xf97 <15850673022@163.com>
2019-11-14 09:33:23 +0800

    update test cases

---
+++ 050/SecurityProblem/integerError/gray contract/gray_truncationError.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.0;
+
+//From Osiris
+
+contract truncationError{
+    mapping(address => uint32) public balances;
+    
+    function receiveEther() public payable{
+        require(msg.value == uint32(msg.value));
+        require(balances[msg.sender] + uint32(msg.value) >= balances[msg.sender]);
+        balances[msg.sender] += uint32(msg.value);
+    }
+}
