c1e433696bdbaf0993da42afbb692884cc2bca38
xf97 <15850673022@163.com>
2019-12-01 12:10:23 +0800

    add new contracts

---
+++ 0421/HiddenTreatsInCode/deprecated/gray contract/gray_deprecated1.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+//from swc
+
+contract DeprecatedSimpleFixed {
+
+    function useDeprecatedFixed() public view {
+        bytes32 bhash = blockhash(0);
+        bytes32 hashofhash = keccak256(bhash);
+        uint gas = gasleft();
+        if (gas == 0) {
+            revert();
+        }
+        address(this).delegatecall();
+        uint8[3] memory a = [1,2,3];
+        (bool x, string memory y, uint8 z) = (false, "test", 0);
+        selfdestruct(address(0));
+    }
+
+    function () external {}
+
+}
