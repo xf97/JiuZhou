3323a4f0fa871c92855038a64260f38880551a44
xf97 <15850673022@163.com>
2019-12-02 10:50:24 +0800

    add dosWithExternalAddr's black contract and gray contract.

--- smartContracts/SecurityProblem/DosWithExternalAddr/black contract/PushButNoPull.sol
+++ smartContracts/SecurityProblem/DosWithExternalAddr/black contract/PushButNoPull.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.21;
+pragma solidity 0.4.26;
 
 //from SWC
 
@@ -7,16 +7,22 @@ contract Refunder {
 address[] private refundAddresses;
 mapping (address => uint) public refunds;
 
-    function Refunder() public{
+    constructor() public{
         refundAddresses.push(0x79B483371E87d664cd39491b5F06250165e4b184);
         refundAddresses.push(0x79B483371E87d664cd39491b5F06250165e4b185);
     }
 
     // bad
     function refundAll() public {
-        for(uint x; x < refundAddresses.length; x++) { // arbitrary length iteration based on how many addresses participated
+        for(uint256 x = 0; x < refundAddresses.length; x++) { // arbitrary length iteration based on how many addresses participated
             require(refundAddresses[x].send(refunds[refundAddresses[x]])); // doubly bad, now a single failure on send will hold up all funds
         }
     }
 
+    //bad too
+    function refundAll_1() public {
+        for(uint256 x = 0; x < refundAddresses.length; x++) { // arbitrary length iteration based on how many addresses participated
+            refundAddresses[x].transfer(refunds[refundAddresses[x]]); // doubly bad, now a single failure on send will hold up all funds
+        }
+    }
 }
