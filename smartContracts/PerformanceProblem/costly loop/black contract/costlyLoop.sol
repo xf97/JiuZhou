1386652b5a9b0822ddb5f78137ae9e8db9573a26
xf97 <15850673022@163.com>
2019-12-03 16:18:15 +0800

    add new contracts

---
+++ smartContracts/PerformanceProblem/costly loop/black contract/costlyLoop.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.0;
+
+//When you add 1 to each element of a large array of integers, break the addition step into several steps.
+
+contract ConstlyLoop{
+    uint256[] public element;   //a dynamic array
+    uint256 public constant addNum = 1;
+    address public owner;
+    
+    constructor() public{
+        owner = msg.sender;
+    }
+    
+    //only owner can write data into this array
+    function appendDate(uint256 _ele) public{
+        require(msg.sender == owner);
+        element.push(_ele);
+    }
+    
+    /*
+    Assuming the array size is already large, adding 1 to each element 
+    of the array at one time will result in a large gas consumption and 
+    may be DOS with the gaslimit
+    */
+    function addOne() public{
+        require(msg.sender == owner);
+        uint256 _length = element.length;
+        for(uint256 i = 0; i < _length; i++)
+            element[i] += 1;
+    }
+}
