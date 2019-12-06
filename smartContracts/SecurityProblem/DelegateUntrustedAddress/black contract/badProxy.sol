4fb93d01d70beded120ad7a358b3269bce1b2b90
xf97 <15850673022@163.com>
2019-12-06 10:25:37 +0800

    add new contracts

---
+++ smartContracts/SecurityProblem/DelegateUntrustedAddress/black contract/badProxy.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.24;
+
+//based on swc
+
+/*
+The address and parameters of the delegate call are controlled by 
+external input, which is dangerous, meaning that an attacker can 
+load any piece of code that is executed in this contract.
+*/
+
+contract Proxy {
+  address public owner;
+
+  constructor() public payable{
+    owner = msg.sender;  
+    require(msg.value > 0);
+  }
+
+  function forward(address callee, bytes memory _data) public {
+    require(callee.delegatecall(_data));
+  }
+  
+  function withdraw() external{
+      require(msg.sender == owner);
+      msg.sender.transfer(address(this).balance);
+  }
+
+}
---
+++ smartContracts/SecurityProblem/DelegateUntrustedAddress/gray contract/gray_badDelegate.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.4.24;
+
+//based on swc
+
+/*
+The address and parameters of the delegate call are controlled by 
+external input, which is dangerous, meaning that an attacker can 
+load any piece of code that is executed in this contract.
+*/
+
+//One solution is to add the white list of delegate addresses
+
+contract gray_Proxy {
+  address public owner;
+  address[] public whiteList;
+
+  constructor() public payable{
+    owner = msg.sender;  
+    require(msg.value > 0);
+  }
+  
+  modifier onlyOwner{
+      require(msg.sender == owner);
+      _;
+  }
+  
+  modifier isTrusted(address _addr){
+      uint256 _length = whiteList.length;
+      bool flag = false;
+      for(uint256 i = 0; i < _length; i++){
+          if(whiteList[i] == _addr){
+              flag = true;
+          }
+      }
+      require(flag);
+      _;   
+  }
+  
+  function addTurstAddr(address _addr) external onlyOwner{
+      whiteList.push(_addr);
+  }
+
+  function forward(address callee, bytes memory _data) public isTrusted(callee){
+    require(callee.delegatecall(_data));
+  }
+  
+  function withdraw() external{
+      require(msg.sender == owner);
+      msg.sender.transfer(address(this).balance);
+  }
+
+}
