dacbb138d757de35ec20fd28af90e7c25aca3931
xf97 <15850673022@163.com>
2019-12-10 11:18:50 +0800

    add new contracts

---
+++ Logical bugs/Prodigal contract/README.md
@@ -0,0 +1,4 @@
+# Prodigal contract
+We call a contract in which anyone can withdraw a deposit a prodigal contract, and the reason for this problem is that the contract does not have access control over the verification of withdrawals, allowing anyone to withdraw ethers from the contract.
+
+In some cases, the problem is caused by misspelled constructors.
---
+++ Logical bugs/Prodigal contract/gray contract/gray_prodigal.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.5.0;
+
+contract gray_prodigal{
+    address public owner;
+    constructor() public payable{
+        owner = msg.sender;
+        require(msg.value > 0);
+    }
+    
+    modifier onlyOnwer{
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function withdraw() external onlyOnwer{
+        msg.sender.transfer(address(this).balance);
+    }
+}
