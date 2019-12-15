337479fad9a5b2e2206d40f2a636498be1d53168
xf97 <15850673022@163.com>
2019-12-15 21:51:25 +0800

    change directoru

---
+++ Smart contract problem classification framework/Uninitialized variables/Uninitialized local variables/problem contract/uninitLocalVar.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.5.0;
+
+//based on slither
+
+contract uninitStateLocalVar{
+    address payable payee;
+    uint256 public _order;
+    event Record(address indexed _donors, uint256 _number, uint256 _money);
+    
+    constructor() public{
+        _order = 0;
+        payee = msg.sender;
+    }
+    //The local variable "number" is not initialized, so "number" does not work.
+    function giveMeMoney() external payable{
+        uint256 number;
+        _order += 1;
+        require(msg.value > 0);
+        emit Record(msg.sender, number, msg.value);
+    }
+
+    function withdrawDonation() external{
+        payee.transfer(address(this).balance);
+    }
+}
--- Smart contract problem classification framework/Uninitialized variables/Uninitialized state variables/problem contract/uninitStateVar.sol
+++ Smart contract problem classification framework/Uninitialized variables/Uninitialized state variables/problem contract/uninitStateVar.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.0;
 
 //based on slither
 
-contract uninitStateLocalVar{
+contract uninitStateVar{
     address payable payee;
     uint256 public _order;
     event Record(address indexed _donors, uint256 _number, uint256 _money);
@@ -10,14 +10,14 @@ contract uninitStateLocalVar{
     constructor() public{
         _order = 0;
     }
-    //The local variable "number" is not initialized, so "number" does not work.
+
     function giveMeMoney() external payable{
-        uint256 number;
+        uint256 number = _order;
         _order += 1;
         require(msg.value > 0);
         emit Record(msg.sender, number, msg.value);
     }
-    //There is no initialized state variable ''payee''', so all 
+    //There is no initialized state variable ''payee'', so all 
     //donations can only be sent to the 0x0 destruction address.
     function withdrawDonation() external{
         payee.transfer(address(this).balance);
