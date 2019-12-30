5e4ad37d30fb562859ab0ee724f73eb9260720b6
xf97 <15850673022@163.com>
2019-12-30 21:12:36 +0800

    add new contracts

---
+++ Smart contract problem classification framework/Incorrect integer operation result/Integer signedness error/misleading contract/gray_IntegerSignednessError.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.5.0;
+
+//based on Osiris
+
+/*
+If A is less than 0, the value of B will be large when A is converted to an 
+unsigned integer B of the same width. Consider withdrawOnce function in this 
+example, enter a negative value will lead to the contract when the balance 
+of a roll out more than 1 ether.
+*/
+
+contract gray_signednessError{
+    mapping(address => bool) public transferred;
+    
+    constructor() public{
+        
+    }
+    
+    function withdrawOnce (int amount) public {
+        //amount is positive, so uint(amount) is also true.
+        require(amount >= 0);
+        if ( amount > 1 ether || transferred [msg.sender]) {
+            revert() ;
+        }
+        msg.sender.transfer(uint(amount));
+        transferred [msg.sender] = true ;
+    }
+}
