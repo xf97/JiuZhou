3b8ff4c7f4d34a92c9df12080131f048478bccbf
xf97 <15850673022@163.com>
2019-12-30 16:45:21 +0800

    add new contracts

---
+++ Smart contract problem classification framework/Accuracy loss/Integer division/contract without the bug/NointegerDivision.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.5.0;
+
+//Use multiplication instead of division
+
+contract NoGetWageNumber {
+    uint256 public coefficient_reciprocal;  //The coefficient_reciprocal is the inverse of the coefficient
+    uint256 public DailyWage;
+    address public boss;
+    
+    constructor() public{
+        DailyWage = 100;
+        coefficient_reciprocal = 3;
+        boss = msg.sender;
+    }
+    
+    modifier onlyOwner{
+        require(msg.sender == boss);
+        _;
+    }
+    
+    function setDailyWage(uint256 _wage) external onlyOwner{
+        DailyWage = _wage;
+    }
+    
+    function setCoefficient(uint256 _co) external onlyOwner{
+        coefficient_reciprocal = _co;
+    }
+    
+    function calculateWage(uint256 dayNumber) view external onlyOwner returns (uint256) {
+        uint256 baseWage = DailyWage * coefficient_reciprocal;
+        return baseWage * dayNumber;
+    }
+}
