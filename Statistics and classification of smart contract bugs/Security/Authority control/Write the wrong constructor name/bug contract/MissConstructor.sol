a312ab80aea0ce0ef6c0516aa3e80633a62790f0
xf97 <15850673022@163.com>
2020-05-29 10:29:59 +0800

    update contracts

--- Statistics and classification of smart contract bugs/Security/Authority control/Write the wrong constructor name/bug contract/MissConstructor.sol
+++ Statistics and classification of smart contract bugs/Security/Authority control/Write the wrong constructor name/bug contract/MissConstructor.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.26;
 
-contract MissContructor{
+contract MissConstructor{
     address public owner;
     
     /*
@@ -10,7 +10,7 @@ contract MissContructor{
     the contract.
     */
     //In some cases, the lack of constructors can be dangerous. If the developer is not going to write a constructor for the contract, the harm of the lack of a constructor is limited to the structural incompleteness of the contract. If the developer intends to write a constructor for the contract, but misspells the function name due to the developer's own negligence, the contract is at great risk. Because in contracts, constructors are often tasked with assigning values to key state variables.
-    function missContructor() public payable{
+    function missConstructor() public payable{
         owner = msg.sender;
         require(msg.value > 0);
     }
